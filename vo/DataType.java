033e43ea20a3a48c7208988c5fdb6bfbeb380f91
anandh2410 <anandh2410@gmail.com>
2015-07-02 22:34:51 -0700

    changes

--- vo/DataType.java
+++ vo/DataType.java
@@ -1,15 +1,33 @@
 package vo;
 
-public enum DataType 
+enum DataTypeName 
 {
 	REAL("REAL"), VARCHAR("VARCHAR"), INTEGER("INTEGER"), SDO_GEOM("SDO_GEOM");
 	private String name;
-	private DataType(String name) 
+	private DataTypeName(String name) 
 	{
 		this.name = name;
 	}
 	public String getDataType()
 	{
 		return name;
+	}
+}
+public class DataType
+{
+	private DataTypeName name;
+	private int size;
+	public DataTypeName getName() 
+	{
+		return name;
+	}
+	public void setName(DataTypeName name) {
+		this.name = name;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
 	}
 }
