6e151ead30a694ff1ed47e1a5589540bb60b7895
nhprasad <neha.h.prasad@gmail.com>
2015-07-07 04:03:54 -0700

    code to bridge the gap between the file access layer and the parser

--- javaminibase/src/vo/SelectQuery.java
+++ javaminibase/src/vo/SelectQuery.java
@@ -6,44 +6,44 @@ import java.util.List;
 
 public class SelectQuery extends Query
 {
-	private List<SelectObj> select_list;
-	private List<FromObj> from_list;
-	private List<CondExpr> cond_list;
+	private List<SelectObj> selectList;
+	private List<FromObj> fromList;
+	private List<CondExpr> condList;
 	public SelectQuery()
 	{
 		super(SELECT, null);
-		select_list = new ArrayList<SelectObj>();
-		cond_list = new ArrayList<CondExpr>();
-		from_list = new ArrayList<FromObj>();
+		selectList = new ArrayList<SelectObj>();
+		condList = new ArrayList<CondExpr>();
+		fromList = new ArrayList<FromObj>();
 	}
 	public void addSelObject(SelectObj obj)
 	{
-		select_list.add(obj);
+		getSelectList().add(obj);
 	}
 	public void addCondObj(CondExpr expr)
 	{
-		cond_list.add(expr);
+		getCondList().add(expr);
 	}
 	public void addFromObj(FromObj obj)
 	{
-		from_list.add(obj);
+		getFromList().add(obj);
 	}
 	@Override
 	public void display()
 	{
 		System.out.println("Select List: ");
-		for(SelectObj obj: select_list)
+		for(SelectObj obj: getSelectList())
 		{
 			System.out.println(obj.toString());
 		}
 		System.out.println("From List: ");
-		for(FromObj temp: from_list)
+		for(FromObj temp: getFromList())
 		{
 			System.out.println(temp.toString());
 		}
 		System.out.println("Conditions: ");
 		boolean fg = false;
-		for(CondExpr expr: cond_list)
+		for(CondExpr expr: getCondList())
 		{
 			if(fg)
 				System.out.println("AND");
@@ -61,4 +61,46 @@ public class SelectQuery extends Query
 			System.out.println();
 		}
 	}
+
+    /**
+     * @return the selectList
+     */
+    public List<SelectObj> getSelectList() {
+        return selectList;
+    }
+
+    /**
+     * @return the fromList
+     */
+    public List<FromObj> getFromList() {
+        return fromList;
+    }
+
+    /**
+     * @return the condList
+     */
+    public List<CondExpr> getCondList() {
+        return condList;
+    }
+
+    /**
+     * @param selectList the selectList to set
+     */
+    public void setSelectList(List<SelectObj> selectList) {
+        this.selectList = selectList;
+    }
+
+    /**
+     * @param fromList the fromList to set
+     */
+    public void setFromList(List<FromObj> fromList) {
+        this.fromList = fromList;
+    }
+
+    /**
+     * @param condList the condList to set
+     */
+    public void setCondList(List<CondExpr> condList) {
+        this.condList = condList;
+    }
 }
